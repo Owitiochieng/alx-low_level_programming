f80351e1d57810dc4256b5af68daa8938413df31
Clifford Owiti <cliffordowiti@gmail.com>
2023-08-10 04:21:21 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Usage of puts function in line printing
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
