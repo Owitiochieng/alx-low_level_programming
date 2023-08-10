70bf9b863a6ed07c5cb3064b8be9388258c9ca92
Clifford Owiti <cliffordowiti@gmail.com>
2023-08-10 12:50:05 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - A program for printing an exact statement to the standard error
+ * Return: 1
+ */
+int main(void)
+{
+fput(stdout, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
