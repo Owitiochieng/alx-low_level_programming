d36350aa94517de4b772fdd6c35f252103bc87a1
Clifford Owiti <cliffordowiti@gmail.com>
2023-08-11 10:21:24 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - A program that prints alphabets in small letters
+ * Return: 0
+ */
+
+int main(void)
+{
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar("\n");
+	return (0);
+}
