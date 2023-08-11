696e0f7c520a67f128e0fc27344c5efe18c5b1f1
Clifford Owiti <cliffordowiti@gmail.com>
2023-08-11 14:26:16 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Lowercase hexadecimals
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+	char b;
+
+	for (a = '0' ; a <= '9' ; a++)
+		putchar(a);
+	for (b = 'a' ; b <= 'f' ; b++)
+		putchar(b);
+
+	putchar('\n');
+	return (0);
+}
