f73fc0b06c456d126a326e38580126ea44ffc7f8
Clifford Owiti <cliffordowiti@gmail.com>
2023-08-16 01:18:20 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,25 +7,19 @@
  */
 int print_sign(int n)
 {
-	if (n >= 49 && n <= 57)
+	if (n > 0)
 	{
-		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
+		_putchar(43);
 		return (1);
 	}
-	else if (n == 48)
+	else if (n == 0)
 	{
-		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
+		_putchar(48);
 		return (0);
 	}
-	else if (n < 48)
+	else if (n < 0)
 	{
-		_putchar('-');
-		_putchar(',');
-		_putchar(' ');
+		_putchar(45);
 		return (-1);
 	}
 	_putchar('\n');
