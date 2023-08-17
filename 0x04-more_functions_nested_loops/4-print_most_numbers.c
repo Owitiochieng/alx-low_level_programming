97689055c00dc5e6b11eb70af2b9a442f78b121a
Clifford Owiti <cliffordowiti@gmail.com>
2023-08-17 07:18:01 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_most_numbers - Selectively prints numbers from 0 to 9
- * Return: void
+ * Return: 0
  */
 void print_most_numbers(void)
 {
@@ -9,8 +9,13 @@ void print_most_numbers(void)
 
 	for (a = 48; a <= 58; a++)
 	{
-		if (a != 50 && a != 52)
-			_putchar(a);
+		if (a != 50)
+		{
+			if (a != 52)
+			{
+				_putchar(a);
+			}
+		}
 	}
 	_putchar('\n');
 }
