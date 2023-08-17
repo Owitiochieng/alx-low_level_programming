e8e780e78e0acdfe1907b53270e840ccd79542ae
Clifford Owiti <cliffordowiti@gmail.com>
2023-08-17 00:09:42 -0700

    Error

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,14 +6,33 @@
 
 int main(void)
 {
-	int a = 1;
-
-	while (a < 100)
+	void printNumbers(void)
 	{
-		putchar((a / 10) + '0');
-		putchar((a % 10) + '0');
+		int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 13, 14, 15, 16, 17, 18, 19, 
+			23, 24, 25, 26, 27, 28, 29, 34, 35, 36, 37, 38, 39, 45, 46, 47, 48,
+		       	49, 56, 57, 58, 59, 67, 68, 69, 78, 79, 89};
+		int numCount = sizeof(numbers) / sizeof(numbers[0]);
+		
+		for (int i = 0; i < numCount; i++)
+		{
+			int tensDigit = numbers[i] / 10;
+			int onesDigit = numbers[i] % 10;
+			
+			putchar('0' + tensDigit);
+			putchar('0' + onesDigit);
+			if (i < numCount - 1)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	
+	int main(void)
+	{
+		printNumbers();
 		putchar('\n');
-		a++;
+		return (0);
 	}
 	return (0);
 }
