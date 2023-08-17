79634230ec8f933697cb1c4a85354698a8421925
Clifford Owiti <cliffordowiti@gmail.com>
2023-08-17 06:37:36 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - Prints numbers from 0 t0 9
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
