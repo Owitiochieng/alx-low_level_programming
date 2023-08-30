267d451d28c0e76b68146f8c515c721a5c01dc55
Clifford Owiti <cliffordowiti@gmail.com>
2023-08-30 01:38:09 -0700

    Updates

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,33 +6,30 @@
 
 int main(void)
 {
-	void printNumbers(void)
+	int a;
+	int b;
+
+	for (a = '0'; a <= '9'; a++)
 	{
-		int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 13, 14, 15, 16, 17, 18, 19, 
-			23, 24, 25, 26, 27, 28, 29, 34, 35, 36, 37, 38, 39, 45, 46, 47, 48,
-		       	49, 56, 57, 58, 59, 67, 68, 69, 78, 79, 89};
-		int numCount = sizeof(numbers) / sizeof(numbers[0]);
-		
-		for (int i = 0; i < numCount; i++)
+		for (b = '0'; b <= '9'; b++)
 		{
-			int tensDigit = numbers[i] / 10;
-			int onesDigit = numbers[i] % 10;
-			
-			putchar('0' + tensDigit);
-			putchar('0' + onesDigit);
-			if (i < numCount - 1)
+			if (!(a > b) || a == b)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(a);
+				putchar(b);
+				if (a == '8' && b == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
-	
-	int main(void)
-	{
-		printNumbers();
-		putchar('\n');
-		return (0);
-	}
+	putchar('\n');
 	return (0);
 }
+
