24fd1496cb4673d5061d15bc25dc64656af2c33d
Clifford Owiti <cliffordowiti@gmail.com>
2023-09-01 12:43:22 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - This function outpus the number of command-line arguments.
+ * @argcount_: The number of command-line arguments.
+ * @arg_values: An array containing the command-line arguments.
+ * Return: 0 by default
+ */
+int main(int argcount_, char *arg_values[])
+{
+	(void) arg_values; /*Ignore arg_values*/
+
+	if (argcount_ == 1)
+	{
+		printf("No arguments were passed.\n");
+	}
+	else
+	{
+		printf("The number of arguments is: %d\n", argcount_ - 1);
+	}
+	return (0);
+}
