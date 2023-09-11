d70cfed443974c524f1cfddaaaaf4e18ba96589a
Clifford Owiti <cliffordowiti@gmail.com>
2023-09-11 11:15:18 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This program is for printing name of
+ * original file for its compilation
+ * Return: 0 at all times
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
