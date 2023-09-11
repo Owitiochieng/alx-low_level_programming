de41b85c95011c887633d5050875afd777c3f337
Clifford Owiti <cliffordowiti@gmail.com>
2023-09-11 10:19:05 -0700

    Preprocessor

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef Cliffs_OBJECT_LIKE_MACRO_H
-#define Cliffs_OBJECT_LIKE_MACRO_H
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 
-#define Cliffs_SIZE 1024
+#define SIZE 1024
 #endif
