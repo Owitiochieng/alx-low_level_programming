2ce613cc79e5676cc21363f83957ef0fdee672f5
Clifford Owiti <cliffordowiti@gmail.com>
2023-10-02 10:55:22 -0700

    Updating 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -249,51 +249,43 @@ void close_elf(int elf)
  */
 int main(int argc, char *argv[])
 {
-    int open_res, read_res;
-    Elf64_Ehdr *header;
+	Elf64_Ehdr *header;
+	int open_res, read_res;
 
-    if (argc != 2)
-    {
-        dprintf(STDERR_FILENO, "Usage: %s <executable>\n", argv[0]);
-        exit(1);
-    }
+	open_res = open(argv[1], O_RDONLY);
+	if (open_res == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		exit(98);
+	}
+	header = malloc(sizeof(Elf64_Ehdr));
+	if (header == NULL)
+	{
+		close_elf(open_res);
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		exit(98);
+	}
+	lseek(open_res, 0, SEEK_SET);
+	read_res = read(open_res, header, sizeof(Elf64_Ehdr));
+	if (read_res == -1)
+	{
+		free(header);
+		close_elf(open_res);
+		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
+		exit(98);
+	}
+	check_elf(header->e_ident);
+	printf("ELF Header:\n");
+	print_magic(header->e_ident);
+	print_class(header->e_ident);
+	print_data(header->e_ident);
+	print_version(header->e_ident);
+	print_osabi(header->e_ident);
+	print_abi(header->e_ident);
+	print_type(header->e_type, header->e_ident);
+	print_entry(header->e_entry, header->e_ident);
 
-    open_res = open(argv[1], O_RDONLY);
-    if (open_res == -1)
-    {
-        perror("open");
-        exit(1);
-    }
-
-    header = malloc(sizeof(Elf64_Ehdr));
-    if (header == NULL)
-    {
-        perror("malloc");
-        close(open_res);
-        exit(1);
-    }
-
-    read_res = read(open_res, header, sizeof(Elf64_Ehdr));
-    if (read_res == -1)
-    {
-        perror("read");
-        free(header);
-        close(open_res);
-        exit(1);
-    }
-
-    check_elf(header->e_ident);
-    printf("ELF Header:\n");
-    print_magic(header->e_ident);
-    print_class(header->e_ident);
-    print_data(header->e_ident);
-    print_version(header->e_ident);
-    print_osabi(header->e_ident);
-    print_abi(header->e_ident);
-    print_type(header->e_type, header->e_ident);
-    print_entry(header->e_entry, header->e_ident);
-
-    free(header);
-    close(open_res);
-    return 0;
+	free(header);
+	close_elf(open_res);
+	return (0);
 }
