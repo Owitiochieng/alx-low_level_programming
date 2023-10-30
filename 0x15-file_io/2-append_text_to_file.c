58c43d8b19d5e86964ddf56e0b4d6afc13e57923
Owitichieng <cliffordowiti@gmail.com>
2023-10-30 08:30:23 -0700

    New updates

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,7 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		for (ncontent = 0; text_content[nletters]; ncontent++)
+		for (ncontent = 0; text_content[ncontent]; ncontent++)
 			;
 
 		ret_val = write(f_des, text_content, ncontent);
